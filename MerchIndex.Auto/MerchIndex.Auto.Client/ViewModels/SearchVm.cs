f24f118e5b247613843d15989efd7c1b9c07e6ba
kourosh <kourosh23@hotmail.com>
2025-03-07 20:32:10 +0100

    companies

---
+++ MerchIndex.Auto/MerchIndex.Auto.Client/ViewModels/SearchVm.cs
@@ -0,0 +1,14 @@
+﻿namespace MerchIndex.Auto.Client.ViewModels
+{
+    public class SearchVm
+    {
+        public string? SearchTerm { get; set; }
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public string? Company { get; set; }
+        public string? Category { get; set; }
+        public string? Manifacturer { get; set; }
+        public string? SortBy { get; set; }
+        public string? OrderBy { get; set; }
+    }
+}
